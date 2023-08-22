9a5785887fff6053312c9b48f2f976358e0ef0bd
Dolapo Adegoju <tichiegoju@gmail.com>
2023-08-22 18:07:40 +0100

    fixing deployments issues

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,8 @@
-pip install -r requirements.txt
+#!/bin/bash
+
+#build the project
+echo "Building the project...."
+python3.9 -m pip install -r requirements.txt
+
+echo "Collect static..."
 python3.9 manage.py collectstatic
