9f5e24937839b4715c595496bd3947d8359ac58e
Dolapo Adegoju <tichiegoju@gmail.com>
2023-08-22 08:37:59 +0100

    fixing deployments issues

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
